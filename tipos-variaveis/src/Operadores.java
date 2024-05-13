a9c9886747ffa9585469e1b39ee47f629547d2ef
Kaue Cepillo <krcepillo@gmail.com>
2024-05-13 21:48:04 +0100

    inclusão do aplicativo Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,62 @@
+public class Operadores {
+  public static void main(String[] args) {
+    // //Operadores Lógicos
+
+    // boolean condicao1 = true;
+    // boolean condicao2 = false;
+
+    // if (condicao1 && condicao2) {
+    //   System.out.println("as duas condições são verdadeiras");
+    // } else if (condicao1 || condicao2) {
+    //   System.out.println("uma das duas condições é verdadeira");
+    // } else {
+    //   System.out.println("nenhuma é verdadeira");
+    // }
+
+    // // Relacionais
+    // int numero1 = 5;
+    // int numero2 = 10;
+
+    // boolean simNao = numero1 == numero2;
+    // System.out.println("numero 1 é igual ao numero 2? " + simNao);
+
+    // simNao = numero1 != numero2;
+    // System.out.println("numero 1 é igual ao numero 2? " + simNao);
+
+    // simNao = numero1 > numero2;
+    // System.out.println("numero 1 é igual ao numero 2? " + simNao);
+
+    // simNao = numero1 >= numero2;
+    // System.out.println("numero 1 é igual ao numero 2? " + simNao);
+
+    // simNao = numero1 < numero2;
+    // System.out.println("numero 1 é igual ao numero 2? " + simNao);
+
+    // simNao = numero1 <= numero2;
+    // System.out.println("numero 1 é igual ao numero 2? " + simNao);
+
+    // String meuNome = "Kaue";
+    // String meuNome2 = new String("Kaue");
+
+    // System.out.println(meuNome == meuNome2);
+    // System.out.println(meuNome.equals(meuNome2)); //compara o conteudo dos objetos
+
+    //Ternario    
+    // int a, b;
+    // a = 10;
+    // b = 10;
+
+    // String resultado = a==b ? "verdadeiro" : "falso";
+    // System.out.println(resultado);
+
+  //   String resultado = "";
+  //   if (a == b) {
+  //     resultado = "verdadeiro";
+  // } else {
+  //     resultado = "falso";
+  // }
+ 
+
+  
+}
+}
