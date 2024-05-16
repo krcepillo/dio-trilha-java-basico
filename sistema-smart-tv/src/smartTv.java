77a1acf5197a1776f1a35702a6f3d3bbb753cdc0
LicaCepillo <liliancepillo@gmail.com>
2024-05-16 22:18:54 +0100

    novo app

--- sistema-smart-tv/src/smartTv.java
+++ sistema-smart-tv/src/smartTv.java
@@ -1,5 +1,14 @@
-public class smartTv {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public class SmartTv {
+
+boolean ligada = false;
+int canal = 1;
+int volume = 25;
+
+public void ligar () {
+    ligada = true;
+}
+
+public void desligar () {
+    ligada = false;
+}
 }
