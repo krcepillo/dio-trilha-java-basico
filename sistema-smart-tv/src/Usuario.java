505430d29992c79a4a9639b7d90a36e65bbdc79d
LicaCepillo <liliancepillo@gmail.com>
2024-05-17 22:22:58 +0100

    delete de arquivos repetidos

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,6 +2,11 @@ public class Usuario {
     public static void main(String[] args) {
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
         System.out.println("TV ligada? " + smartTv.ligada);
         System.out.println("Canal atual? " + smartTv.canal);
         System.out.println("Volume atual? " + smartTv.volume);
