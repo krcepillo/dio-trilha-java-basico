615ec4de99ab945cd99e684181c3214fe04df2d2
LicaCepillo <liliancepillo@gmail.com>
2024-05-21 20:03:36 +0100

    varios arquivos

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,23 @@
+ 
+    /**
+     * <h1>Calculadora</h1>
+     * A calculadora realiza operações matemáticas entre numeros inteiros
+     * <p>
+     * <b>Note:</b> Leia atentamente a documentação desta classes 
+     * para desfrutar dos recursos oferecidos pelo autor
+     * 
+     * @author Kauê Cepillo
+     * @version 1.0
+     * @since 01/01/2000
+     */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois  este é o primeiro parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar (int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+}
---
+++ Calculadora/src/Comentarios.java
@@ -0,0 +1,23 @@
+public class Comentarios {
+
+    public static void main(String[] args) {
+        // Olá eu sou um comentário de apenas uma linha
+
+        /* Olá,
+         * Eu sou um comentário de várias 
+         * linhas
+         */
+    }
+    
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação
+     * Que incrível!!
+     */
+
+    public static void metodo(){
+        
+    }
+}
