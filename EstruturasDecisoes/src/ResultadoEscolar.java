58e71a8013d404bfa8b5e42898d0e5b4aeaa8024
LicaCepillo <liliancepillo@gmail.com>
2024-05-29 22:58:09 +0100

    novos arquivos java

--- EstruturasDecisoes/src/ResultadoEscolar.java
+++ EstruturasDecisoes/src/ResultadoEscolar.java
@@ -1,11 +1,22 @@
 public class ResultadoEscolar {
     
     public static void main(String[] args) {
-        int nota = 7;
-
+        int nota = 6;
+        /*
         if(nota >=7)
             System.out.println("Aprovado");
-        else
+
+         else if (nota >= 5 && nota < 7) 
+            System.out.println("Recuperação");                
+            
+         else
             System.out.println("Reprovado");
+        */
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
     }
+
+
 }
