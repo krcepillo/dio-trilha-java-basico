d0fb2919f374af3eb0a052d27e7d6a0b778ceb39
LicaCepillo <liliancepillo@gmail.com>
2024-05-30 15:11:31 +0100

    exemplos de estrutura de repetição

---
+++ Estrutura de Repetições/src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneiro = 1; carneiro <= 5; carneiro ++) {
+            System.out.println(carneiro + " carneirinho");
+        }
+    }
+}
---
+++ Estrutura de Repetições/src/ForArray.java
@@ -0,0 +1,5 @@
+public class ForArray {
+    public static void main(String[] args) {
+        
+    }
+}
